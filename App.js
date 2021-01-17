60be7d257b2b6aad66b4495998a80209a3b4c22c
Tara <75340062+traopand@users.noreply.github.com>
2021-01-17 07:52:24 -0500

    rally app files

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, Button } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// Components
+import Home from "./Components/Home";
+import Profile from "./Components/Profile";
+import AppSearch from "./Components/AppSearch";
+import ProtestPage from "./Components/ProtestPage";
+import ConvertedPage1 from "./Components/ConvertedPage1";
+import ConvertedPage2 from "./Components/ConvertedPage2";
+import ConvertedPage3 from "./Components/ConvertedPage3";
+import ConvertedPage4 from "./Components/ConvertedPage4";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const Navigator = createStackNavigator({
+  Home: { screen: Home },
+  Profile: { screen: Profile },
+  AppSearch: { screen: AppSearch },
+  ProtestPage: { screen: ProtestPage },
+  ConvertedPage1: { screen: ConvertedPage1 },
+  ConvertedPage2: { screen: ConvertedPage2 },
+  ConvertedPage3: { screen: ConvertedPage3 },
+  ConvertedPage4: { screen: ConvertedPage4 },
 });
+
+const App = createAppContainer(Navigator);
+
+export default App;
